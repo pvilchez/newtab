5a39a994641a1a86e7c88c8e7bf4c432b987efc0
Paul Vilchez <vilchez.paul@gmail.com>
2011-07-03 20:24:45 -0400

    more robust notifications; styled on page as well.

--- scripts/fetch.js
+++ scripts/fetch.js
@@ -16,17 +16,25 @@ xhr.onreadystatechange = function () {
 						if (resp.data.link_karma != temp){
 							var difference = resp.link_karma - temp;
 							console.log("[reddit] Link Karma: " + difference);
+							renderNotification("reddit | link karma: " + difference);
 						}
 						temp = localStorage.getItem("reddit_comment_karma");
 						if (resp.data.comment_karma != temp){
 							var difference = resp.link_karma - temp;
 							console.log("[reddit] Comment Karma: " + difference);
+							renderNotification("reddit | comment karma: " + difference);
 						}
+						localStorage.removeItem("reddit_link_karma");
+						localStorage.removeItem("reddit_comment_karma");
 						localStorage.setItem("reddit_link_karma", resp.data.link_karma);
 						localStorage.setItem("reddit_comment_karma", resp.data.comment_karma);
 						console.log("[reddit] Unread Mail: " + resp.data.has_mail);
+						if (resp.data.has_mail){
+							renderNotification("reddit | orangered");
+						}
+						
 					} catch (e) {
-						if (e == QUOTA_EXCEEDED_ERR) {
+						if (e === 'QUOTA_EXCEEDED_ERR') {
 							console.log('Quota exceeded!'); 
 						}
 					}
@@ -56,10 +64,12 @@ hn_xhr.onreadystatechange = function () {
 						if (resp.karma != temp){
 							var difference = resp.karma - temp;
 							console.log("[HN] Karma: " + difference);
+							renderNotification("hn | karma: " + difference);
 						}
+						localStorage.removeItem("hn_karma", resp.karma);
 						localStorage.setItem("hn_karma", resp.karma);
 					} catch (e) {
-						if (e == QUOTA_EXCEEDED_ERR) {
+						if (e === 'QUOTA_EXCEEDED_ERR') {
 							console.log('Quota exceeded!'); 
 						}
 					}
@@ -72,7 +82,7 @@ hn_xhr.onreadystatechange = function () {
 hn_xhr.send();
 
 var forrst_xhr = new XMLHttpRequest();
-forrst_xhr.open("GET", "http:/forrst.com/feed/poll?u="+username, true);
+forrst_xhr.open("GET", "http://forrst.com/feed/poll?u="+username, true);
 forrst_xhr.onreadystatechange = function () {
 	'use strict';
 	if (forrst_xhr.readyState === 4) {
@@ -81,7 +91,8 @@ forrst_xhr.onreadystatechange = function () {
 			if (resp === 0){
 				console.log("[forrst] zero notifications");	
 			} else if (resp > 0){
-				console.log("[forrst] unread notification(s): " + resp);	
+				console.log("[forrst] unread notification(s): " + resp);
+				renderNotification("forrst | unread notification(s): " + resp);
 			} else {
 				console.log("wtf?");	
 			}
@@ -90,3 +101,25 @@ forrst_xhr.onreadystatechange = function () {
 	}
 };
 forrst_xhr.send();
+
+function renderNotification(notification){
+	console.log("rendering...");
+	var attn = document.getElementById("right-sidebar-list");
+	var li = document.createElement("li");
+	var a = document.createElement("a");
+	var t = document.createTextNode(notification);
+	var s = document.createElement("div");
+	t.data = notification;
+	console.log(t.data);
+	
+	a.addEventListener("click", function() {
+		attn.removeChild(li);
+	}, false);
+	
+	a.textContent = " [x] ";
+	
+	li.appendChild(t);
+	li.appendChild(a);
+	li.appendChild(s);
+	attn.appendChild(li);	
+}
